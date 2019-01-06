e42905e1d74fa959ca621cabb465250561314100
johnthebrit <john@savilltech.com>
2019-01-06 15:50:59 -0600

    End of Module 1 Updates

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # PowerShellMC
 PowerShell Master Class Assets
 
+All content Copyright 2019 John Savill. All rights reserved
+No part of this course be used without express permission from the author
+john@savilltech.com
+@NTFAQGuy
+https://savilltech.com
+
 YouTube Playlist -
 
 Useful links
@@ -11,3 +17,7 @@ Git - https://git-scm.com/downloads
 
 Chocolatey Install -
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Chocolately PowerShell Core upgrade -
+Choco outdated
+Choco upgrade powershell-core
--- Sample.ps1
+++ Sample.ps1
@@ -5,16 +5,44 @@ dir | Sort-Object lastwritetime
 
 dir | sort-object –descending –property lastwritetime
 
-dir | foreach {"$($_.GetType().fullname)  -  $_.name"}
+#To show the object types being passed
+dir | foreach {"$($_.GetType().fullname)  -  $_.name"}  #lazy quick version using alias
+Get-ChildItem | ForEach-Object {"$($_.GetType().fullname)  -  $_.name"}  #Proper script version
 #endregion
 
+#------------------------------
 
+#Region Modules
+Get-Module #to see those loaded
+Get-Module –listavailable #to see all available
+Import-Module <module>  #to add into PowerShell instance
+Get-Command –Module <module> #to list commands in a module
+Get-Command -Module <module> | Select-Object -Unique Noun | Sort-Object Noun
+Get-Command -Module <module> | Select -Unique Noun | Sort Noun  #Lazy version :-)
+
+(Get-Module <module name>).Version  #make sure module has been imported first or will not get output
+(Get-Module az.compute).Version
+Install-Module Az
+Update-Module Az
+#endregion
+
+#------------------------------
+
+#Region Help
+Get-Command –Module <module>
+Get-Command –Noun <noun>
+Update-Help
+#endregion
+
+#------------------------------
 
 #region Hello World
 Write-Output "Hello World"
 
+#Use a variable
 $name = "John"
 Write-Output "Hello $name"
 
+#Use an environment variable
 Write-Output "Hello $env:USERNAME"
 #endregion
