e2672ab1d6b6615536185458064d751961b06415
John Savill <john@savilltech.com>
2019-01-20 05:48:18 -0600

    Update

--- Assets/SavillTechWebBase.ps1
+++ Assets/SavillTechWebBase.ps1
@@ -1,61 +1,21 @@
-﻿
-#Set-ExecutionPolicy unrestricted -Force
-#Enable-PSRemoting -Force
-
-#uses https://www.powershellgallery.com/packages/xWebAdministration/2.4.0.0
-Install-Module -Name xWebAdministration
-
-Configuration SavillTechWebsite
+﻿configuration WebConfig
 {
-    param
-    (
-        # Target nodes to apply the configuration
-        [string[]]$NodeName = 'localhost'
-    )
-    Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
-    # Import the module that defines custom resources
-    Import-DscResource -Module xWebAdministration
-    Node $NodeName
+    Node IsWebServer
     {
-        # Install the IIS role
         WindowsFeature IIS
         {
-            Ensure          = "Present"
-            Name            = "Web-Server"
+            Ensure               = 'Present'
+            Name                 = 'Web-Server'
+            IncludeAllSubFeature = $true
         }
-        #Install ASP.NET 4.5
-        WindowsFeature ASPNet45
+    }
+
+    Node NotWebServer
+    {
+        WindowsFeature IIS
         {
-          Ensure = “Present”
-          Name = “Web-Asp-Net45”
-        }
-        # Stop the default website
-        xWebsite DefaultSite
-        {
-            Ensure          = "Present"
-            Name            = "Default Web Site"
-            State           = "Stopped"
-            PhysicalPath    = "C:\inetpub\wwwroot"
-            DependsOn       = "[WindowsFeature]IIS"
-        }
-        # Copy the website content
-        File WebContent
-        {
-            Ensure          = "Present"
-            SourcePath      = "C:\Source\SavillSite"
-            DestinationPath = "C:\inetpub\SavillSite"
-            Recurse         = $true
-            Type            = "Directory"
-            DependsOn       = "[WindowsFeature]AspNet45"
-        }
-        # Create a new website
-        xWebsite SavTechWebSite
-        {
-            Ensure          = "Present"
-            Name            = "SavillSite"
-            State           = "Started"
-            PhysicalPath    = "C:\inetpub\SavillSite"
-            DependsOn       = "[File]WebContent"
+            Ensure               = 'Absent'
+            Name                 = 'Web-Server'
         }
     }
 }
